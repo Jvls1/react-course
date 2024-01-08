00afa9ed65db6ce0852cb184d6e87890ef2564db
Joao Vitor <joaovlopes000@gmail.com>
2024-01-08 13:56:09 -0300

    finished tic tac toe

--- 07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
+++ 07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
@@ -1,12 +1,4 @@
-import { useState } from "react"
-
-const initialGameBoard = [
-  [null, null, null],
-  [null, null, null],
-  [null, null, null]
-]
-
-export default function GameBoard({ onSelectSquare }) {
+export default function GameBoard({ onSelectSquare, board }) {
   // const [gameBoard, setGameBoard] = useState(initialGameBoard)
 
   // function handleSelectSquare(rowIndex, colIndex) {
@@ -25,12 +17,14 @@ export default function GameBoard({ onSelectSquare }) {
 
   return (
     <ol id="game-board">
-      {gameBoard.map((row, rowIndex) => (
+      {board.map((row, rowIndex) => (
         <li key={rowIndex}>
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button onClick={onSelectSquare}>
+                <button
+                  onClick={() => onSelectSquare(rowIndex, colIndex)}
+                  disabled={playerSymbol !== null}>
                   {playerSymbol}
                 </button>
               </li>
