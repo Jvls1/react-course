7c73f48bd90e851d9648dadcf1cba1641861f0f2
Joao Vitor <joaovlopes000@gmail.com>
2024-01-09 14:26:23 -0300

    test react with Jest

---
+++ 27-jest-project/src/App.js
@@ -0,0 +1,13 @@
+import Greeting from './components/Greeting'
+import './App.css';
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <Greeting />
+    </div>
+  );
+}
+
+export default App;
